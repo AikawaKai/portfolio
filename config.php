08523e7b24c1179739f0971a7001c91775ffc370
AikawaKai <marco.odore@gmail.com>
2015-08-24 19:37:52 +0200

    issue3 bug fixed

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php//generali
+ 
+$sito_internet = "Targetweb";
+ 
+$data =(date("d-m-y"));
+ 
+$vers = "2.0";
+ 
+//URL PER HTACCESS
+ 
+$base_url = "http://marcoodore.ddns.net";
+ 
+//connessione DB
+ 
+$host = "localhost";
+ 
+$db_user = "postgres";
+ 
+$db_psw = "postgres";
+ 
+$db_name = "target";
+?>
---
+++ privato.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+    <title>Zona Privata</title>
+ 
+    <!--Pannello di gestione creato da Mel Riccardo-->
+    
+ 
+</head>
+<body>
+ 
+    Zona Privata
+ 
+</body>
+</html>
